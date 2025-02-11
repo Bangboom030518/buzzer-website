ee0b10d634b2cd1a58a0ad500ff8b394d637b99b
Charlie Clift <bang.boom030518@gmail.com>
2025-02-11 19:23:18 +0000

    fix: rounding

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const btnStart = document.querySelector("#timer-button");
 const timestamp = document.querySelector("#timestamp");
+const output = document.querySelector("#output");
 
 let currentBuzzer;
 
@@ -12,23 +13,31 @@ function random(min, max) {
 }
 
 function round(number) {
-  return Math.round((number + Number.EPSILON) * 100) / 100
+  return Math.round((number + Number.EPSILON) * 100) / 100;
 }
 
 btnStart.addEventListener("click", () => {
+  btnStart.style.display = "none";
+  document.body.style.animation = "rainbow 5s infinite linear";
   const interval = 100;
-  let time = Math.floor(random(3000, 8000));
-  timestamp.innerText = time / 1000;
+  let time = 10000;
+  timestamp.innerText = (time / 1000).toFixed(1);
   let loop = setInterval((stop) => {
+    new Audio("../../CupcakKe/Clips/gulp.mp3").play();
     if (currentBuzzer) {
-      console.log(`buzzer ${currentBuzzer} won`);
+      output.textContent = `buzzer ${currentBuzzer} won`;
       clearInterval(loop);
+      btnStart.style.display = "";
+      document.body.style.animation = "";
+      setTimeout(() => (output.textContent = ""), 3000);
     }
     time -= interval;
     if (time <= 0) {
       time = 0;
       clearInterval(loop);
+      btnStart.style.display = "";
+      document.body.style.animation = "";
     }
-    timestamp.innerText = time / 1000;
+    timestamp.innerText = (time / 1000).toFixed(1);
   }, interval);
 });
